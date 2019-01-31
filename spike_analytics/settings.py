e3e004e7fc3283ec6ac69b5f31f08e2b3f18ff6e
z_goodman <zgoodman@live.unc.edu>
2019-01-30 23:08:31 -0500

    began back end work to register users

--- spike_analytics/settings.py
+++ spike_analytics/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'spike_stats.apps.SpikeStatsConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
